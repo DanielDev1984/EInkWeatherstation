cd09cf94110545a0b2e0090824b0fb93cbcccc60
daniel@raspberry <straight_morris@web.de>
2024-06-09 10:54:06 +0200

    add basic weathreapi docu and basic response formatting

--- src/provideIcon.py
+++ src/provideIcon.py
@@ -1,15 +1,29 @@
+import requests
 from updateSvg import updateSvg
 from weatherprovider import getWeather
 
 def main():
     print(f"main called")
-    output_dict = {
+    
+url = "https://api.brightsky.dev/weather?date=2024-06-09&lat=47.56&lon=7.79"
+
+#querystring = {"date":"2024-06-01","last_date":"2023-08-08","lat":"52.52","lon":"13.4"}
+querystring = {""}
+
+headers = {"Accept": "application/json"}
+
+response = requests.get(url)
+
+for key in response.json()["weather"]:
+    print(key["timestamp"] + " " + str(key["temperature"]))
+
+'''output_dict = {
         'WEATHER_ICON': getWeather()
     }
     # todo: use relative path
-    template_svg_filename = "/home/daniel/development/eink/EInkWeatherstation/pic/screen-template.svg"
-    output_svg_filename = 'screen-output-weather.svg'
-    updateSvg(template_svg_filename,output_dict, output_svg_filename)
+template_svg_filename = "/home/daniel/development/eink/EInkWeatherstation/pic/screen-template.svg"
+output_svg_filename = 'screen-output-weather.svg'
+updateSvg(template_svg_filename,output_dict, output_svg_filename)'''
 
 print(f"call main")
 
